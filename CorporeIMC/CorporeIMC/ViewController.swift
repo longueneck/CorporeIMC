bccf79db3e78cc1637624d0aaebdf06995eb959d
longueneck <kevinlongue@hotmail.com>
2023-02-01 14:01:01 -0300

    Implementando UI no codigo

--- CorporeIMC/CorporeIMC/ViewController.swift
+++ CorporeIMC/CorporeIMC/ViewController.swift
@@ -18,9 +18,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var SelectGender: UISegmentedControl!
     
+    @IBOutlet weak var images: UIImageView!
     var humano: Humano?
     
     override func viewDidLoad() {
+        let selectedTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 67/255, green: 150/255, blue: 213/255, alpha: 1.0) ]
+        let normalTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+
+        SelectGender.setTitleTextAttributes(selectedTitleTextAttributes, for: .selected)
+        SelectGender.setTitleTextAttributes(normalTitleTextAttributes, for: .normal)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -29,11 +35,9 @@ class ViewController: UIViewController {
     @IBAction func calculate(_ sender: UIButton) {
         
         if SelectGender.selectedSegmentIndex == 0{
-            humano = Humano(gender: "M", peso: Double(tfPeso.text!)!, altura: Double(tfAltura.text!)!)
-            lbResultado.text = humano?.calculateMale()
+            lbResultado.text = showResultMale()
         }else{
-            humano = Humano(gender: "F", peso: Double(tfPeso.text!)!, altura: Double(tfAltura.text!)!)
-            lbResultado.text = humano?.calculateFemale()
+            lbResultado.text = showResultFemale()
             
         }
     }
@@ -42,6 +46,56 @@ class ViewController: UIViewController {
             
         }
         
-        
-    
+    func showResultMale() -> String{
+        var resultadoMale: String = ""
+        if let altura = Double(tfAltura.text!), let peso = Double(tfPeso.text!) {
+            let contaMale = peso / (altura * altura)
+            
+            switch contaMale {
+            case 17.9...18.9:
+                resultadoMale = "Magresa"
+                images.image = UIImage(named: "magreza")
+            case 19...24.9:
+                resultadoMale = "Peso Ideal"
+                images.image = UIImage(named: "ideal")
+            case 25...27.7:
+                resultadoMale = "Acima do Peso"
+                images.image = UIImage(named: "sobre")
+            case 27.8...29.8:
+                resultadoMale = "Obesidade"
+                images.image = UIImage(named: "obesidade")
+            default:
+                resultadoMale = "Obesidade Extrema"
+                images.image = UIImage(named: "obesidade")
+                
+            }
+        }
+        return resultadoMale
+    }
+    func showResultFemale() -> String{
+        var resultadoFemale: String = ""
+        if let altura = Double(tfAltura.text!), let peso = Double(tfPeso.text!) {
+            let contaFemale = peso / (altura * altura)
+            
+            switch contaFemale {
+            case 15...17.9:
+                resultadoFemale = "Magresa"
+                images.image = UIImage(named: "magreza")
+            case 18...24.4:
+                resultadoFemale = "Peso Ideal"
+                images.image = UIImage(named: "ideal")
+            case 24.5...27.2:
+                resultadoFemale = "Acima do Peso"
+                images.image = UIImage(named: "sobre")
+            case 27.3...29.3:
+                resultadoFemale = "Obesidade"
+                images.image = UIImage(named: "obesidade")
+            default:
+                resultadoFemale = "Obesidade Extrema"
+                images.image = UIImage(named: "obesidade")
+                
+            }
+        }
+        return resultadoFemale
+    }
 }
--- CorporeIMC/CorporeIMC/humano.swift
+++ CorporeIMC/CorporeIMC/humano.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import UIKit
+
+
 
 class Humano {
     
@@ -19,53 +22,5 @@ class Humano {
         self.altura = altura
     }
     
-    func calculateMale() -> String{
-        var resultadoMale: String?
-        if gender == "M"{
-            altura *= altura
-            var contaMale = peso / altura
-            
-            switch contaMale {
-            case 17.9...18.9:
-                print("Magresa");
-                resultadoMale = "Magresa"
-            case 19...24.9:
-                print("Peso Ideal");
-                resultadoMale = "Peso Ideal"
-            case 25...27.7:
-                print("Acima do Peso");
-                resultadoMale = "Acima do Peso"
-            case 27.8...29.8:
-                print("Obesidade");
-                resultadoMale = "Obesidade"
-            default:
-                print("Obesidade Extrema");
-                resultadoMale = "Obesidade Extrema"
-                
-            }
-        }
-        return resultadoMale!
-    }
-    func calculateFemale() -> String{
-        var resultadoFemale: String?
-        if gender == "F"{
-            altura *= altura
-            var contaFemale = peso / altura
-            
-            switch contaFemale {
-                case 15...17.9:
-                    print("Magresa");
-                case 18...24.4:
-                    print("Peso Ideal");
-                case 24.5...27.2:
-                    print("Acima do Peso");
-                case 27.3...29.3:
-                    print("Obesidade");
-                default:
-                    print("Obesidade Extrema");
-                
-            }
-        }
-        return resultadoFemale!
-    }
+    
 }
